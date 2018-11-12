fc6afee47de5bc3b10c0c409971e44db754803a7
NirLondon <39864310+NirLondon@users.noreply.github.com>
2018-11-12 13:47:58 +0200

    optimalpackage

---
+++ Cellular/OptimalPackage.DAL/OptimalPackageRepository.cs
@@ -0,0 +1,26 @@
+﻿using Cellular.Common.DTOs;
+using Cellular.MainDal;
+using OptimalPackage.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OptimalPackage.DAL
+{
+    class OptimalPackageRepository : IOptimalPackageRepository
+    {
+        public IEnumerable<Call> GetCallsOf(int lineId, DateTime from, DateTime to)
+        {
+            using (var context = new CellularContext())
+            {
+                return context.Calls.Where(c => c.LineId==lineId&& c.)
+            }
+        }
+
+        public IEnumerable<Sms> GetSmsesOf(int lineId, DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
