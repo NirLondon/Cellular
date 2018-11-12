e998d4dcbeaea5ffa80633b70e91c53de6236961
omercoen050 <omercoen050@gmail.com>
2018-11-12 13:06:34 +0200

    fff

---
+++ Cellular/CRM.Common/Interfaces/ILinesRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cellular.CRM.Common.Interfaces
+{
+    interface ILinesRepository
+    {
+    }
+}
---
+++ Cellular/Cellular.Server/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using Cellular.CRM.Common.Interfaces;
+using CRM.Bl;
+using CRM.Dal;
+using SimpleInjector;
+using SimpleInjector.Integration.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Cellular.Server
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            // config container and registering
+            var container = new Container();
+            container.Register<IClientsManager, ClientManager>();
+            container.Register<IClientsRepository, ClientRepository>();
+            container.RegisterMvcControllers();
+
+            // Makes sure that the container configed properly
+            container.Verify();
+            // Set the app dependency resolver to this container
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+    }
+}
